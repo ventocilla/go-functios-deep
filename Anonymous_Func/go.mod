04edf3d3906cbe93c0cc08740eedc8ffecec6f97
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-04-05 20:47:13 -0300

    dir correction names

---
+++ Anonymous_Func/go.mod
@@ -0,0 +1,3 @@
+module example.com/func_deep
+
+go 1.22.1
---
+++ Functionarevalues/main.go
@@ -0,0 +1,55 @@
+package functionarevalues
+
+import "fmt"
+
+type transformFn func(int) int
+
+//type anotherFn func(int, []string, map[string][int]([]int, string)
+
+func main() {
+	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+	fmt.Println(doubled)
+	fmt.Println(tripled)
+	transformerFn1 := getTransformedFunction(&numbers)
+	transformerFn2 := getTransformedFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformerFn1)
+	moreTransformedNumbers := transformNumbers(&numbers, transformerFn2)
+
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformedNumbers)
+}
+
+func doubleNumbers(numbers *[]int) []int {
+	dNumbers := []int{}
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, double(val*2))
+	}
+	return dNumbers
+}
+
+func transformNumbers(numbers *[]int, transform transformFn) []int {
+	dNumbers := []int{}
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
+	}
+	return dNumbers
+}
+
+func getTransformedFunction(numbers *[]int) func(int) int {
+	if (*numbers)[0] == 1 {
+		return double
+	}
+	return triple
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
+}
