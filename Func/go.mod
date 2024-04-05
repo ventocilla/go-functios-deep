7eece93f63b70f7c934e4d115c61b56811e91182
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-04-05 20:43:20 -0300

    intro anoynmous functions

--- Func/go.mod
+++ Func/go.mod
@@ -1,3 +1,3 @@
-module example.com/func
+module example.com/func_deep
 
 go 1.22.1
--- Func/main.go
+++ Func/main.go
@@ -2,54 +2,22 @@ package main
 
 import "fmt"
 
-type transformFn func(int) int
-
-//type anotherFn func(int, []string, map[string][int]([]int, string)
-
 func main() {
-	numbers := []int{1, 2, 3, 4}
-	moreNumbers := []int{5, 1, 2}
-	doubled := transformNumbers(&numbers, double)
-	tripled := transformNumbers(&numbers, triple)
-	fmt.Println(doubled)
-	fmt.Println(tripled)
-	transformerFn1 := getTransformedFunction(&numbers)
-	transformerFn2 := getTransformedFunction(&moreNumbers)
+	numbers := []int{1, 2, 3}
 
-	transformedNumbers := transformNumbers(&numbers, transformerFn1)
-	moreTransformedNumbers := transformNumbers(&numbers, transformerFn2)
+	transformed := transformNumbers(&numbers, func(number int) int {
+		return number * 2
+	})
 
-	fmt.Println(transformedNumbers)
-	fmt.Println(moreTransformedNumbers)
+	fmt.Println(transformed)
 }
 
-func doubleNumbers(numbers *[]int) []int {
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
-	for _, val := range *numbers {
-		dNumbers = append(dNumbers, double(val*2))
-	}
-	return dNumbers
-}
 
-func transformNumbers(numbers *[]int, transform transformFn) []int {
-	dNumbers := []int{}
 	for _, val := range *numbers {
 		dNumbers = append(dNumbers, transform(val))
 	}
+
 	return dNumbers
-}
-
-func getTransformedFunction(numbers *[]int) func(int) int {
-	if (*numbers)[0] == 1 {
-		return double
-	}
-	return triple
-}
-
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number int) int {
-	return number * 3
 }
